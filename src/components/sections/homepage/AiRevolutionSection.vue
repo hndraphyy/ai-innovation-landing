e4d996903a4b34cd56bce5ee6a7fc32a55a1363c
hndraphyy <hendraaditya1913@gmail.com>
2025-12-30 08:42:39 +0700

    style: add translate animate in ai revolution img

--- src/components/sections/homepage/AiRevolutionSection.vue
+++ src/components/sections/homepage/AiRevolutionSection.vue
@@ -2,6 +2,18 @@
 import type { aiRevolution } from '@/constants/homepage/aiRevolutionContent'
 
 defineProps<aiRevolution>()
+
+const getImgStyle = (index: number) => {
+  const common = 'transition-all duration-500 ease-in-out'
+  const styles = [
+    `${common} -mr-1.5 md:-mr-4 z-2 hover:z-4 hover:scale-[1.08] hover:-rotate-13`,
+    `${common} -mx-1.5 md:-mx-6 z-1 hover:z-4 hover:scale-[1.08] hover:-rotate-13`,
+    `${common} -mx-2 md:-mx-8 z-2 hover:z-4 hover:scale-[1.08] hover:-rotate-13`,
+    `${common} -mx-1.5 md:-mx-6 z-3 hover:z-5 hover:scale-[1.08] hover:rotate-13`,
+    `${common} -ml-1.5 md:-ml-4 z-4 hover:z-4 hover:scale-[1.08] hover:rotate-13`,
+  ]
+  return styles[index] || ''
+}
 </script>
 
 <template>
@@ -11,15 +23,21 @@ defineProps<aiRevolution>()
         <h1 class="text-heading m-auto leading-[30px] md:leading-[64px] lg:w-200">
           {{ title }}
           <span
-            class="px-5 border border-brand-dark rounded-full bg-gradient-to-b from-[#F0EEFB] from-30% to-[#FFFFFF] to-61% text-brand-dark font-medium inline-block"
+            class="text-heading px-2 md:px-5 border border-brand-dark rounded-full bg-gradient-to-b from-[#F0EEFB] from-30% to-[#FFFFFF] to-61% text-brand-dark font-medium inline-block"
           >
             {{ spanTitle }}
           </span>
         </h1>
-        <p class="text-par m-auto lg:w-200 pt-5 pb-8 md:pb-15">{{ description }}</p>
+        <p class="text-par m-auto lg:w-200 pt-5 lg:pb-15">{{ description }}</p>
       </div>
-      <div class="w-full">
-        <img :src="image" alt="Ai-Revolution-Image" />
+      <div
+        class="flex justify-center items-end gap-2 md:gap-4 overflow-hidden py-10 md:px-10 transition-transform duration-500"
+      >
+        <div v-for="(img, index) in images" :key="index" class="" :class="getImgStyle(index)">
+          <div class="overflow-hidden">
+            <img :src="img" :alt="`ai-img-${index}`" class="w-full h-full object-cover" />
+          </div>
+        </div>
       </div>
     </main>
   </section>
--- src/constants/homepage/aiRevolutionContent.ts
+++ src/constants/homepage/aiRevolutionContent.ts
@@ -1,10 +1,14 @@
-import ImageAsset from '@/assets/images/homepage/ai-revolution.webp'
+import Ai1 from '@/assets/images/homepage/ai-img-1.webp'
+import Ai2 from '@/assets/images/homepage/ai-img-2.webp'
+import Ai3 from '@/assets/images/homepage/ai-img-3.webp'
+import Ai4 from '@/assets/images/homepage/ai-img-4.webp'
+import Ai5 from '@/assets/images/homepage/ai-img-5.webp'
 
 export interface aiRevolution {
   title: string
   spanTitle: string
   description: string
-  image: string
+  images: string
 }
 
 export const AI_REVOLUTION_CONTENT = {
@@ -12,5 +16,5 @@ export const AI_REVOLUTION_CONTENT = {
   spanTitle: 'Al Revolution',
   description:
     'Our mission is to empower businesses with cutting-edge Al technologies that enhance performance, streamline operations, and drive growth. We believe in the transformative potential of Al and are dedicated to making it accessible to businesses of all sizes, across all industries.',
-  image: ImageAsset,
+  images: [Ai1, Ai2, Ai3, Ai4, Ai5],
 }
