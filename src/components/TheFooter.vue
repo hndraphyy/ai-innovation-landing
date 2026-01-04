a53526d2d735b3595e4847a60412e7432fe01bfb
hndraphyy <hendraaditya1913@gmail.com>
2026-01-04 23:30:07 +0700

    feat: setup footer

---
+++ src/components/TheFooter.vue
@@ -0,0 +1,20 @@
+<script setup lang="ts">
+import BrandLogo from '@/assets/icon/logo-brand.svg'
+</script>
+
+<template>
+  <footer>
+    <main>
+      <div>
+        <div>
+          <div>
+            <img :src="BrandLogo" alt="" />
+            <span></span>
+          </div>
+        </div>
+        <div></div>
+      </div>
+      <div></div>
+    </main>
+  </footer>
+</template>
