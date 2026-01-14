bdb4adef4ae434972500b1e7c3362ddf61af6f83
hndraphyy <hendraaditya1913@gmail.com>
2026-01-14 19:33:06 +0700

    feat: completed service section on about page

--- src/components/sections/aboutpage/ServiceSection.vue
+++ src/components/sections/aboutpage/ServiceSection.vue
@@ -11,14 +11,24 @@ defineProps<ServiceContent>()
       <div class="flex justify-between items-center flex-col md:flex-row">
         <div class="text-center md:text-start">
           <h1 class="text-heading">{{ title }}</h1>
-          <p class="text-par max-w-[667px] pt-6">{{ description }}</p>
+          <p class="text-par md:max-w-[507px] lg:max-w-[667px] pt-6">{{ description }}</p>
         </div>
-        <div class="pt-10 md:pt-0">
+        <div class="pt-10 lg:pt-0">
           <Button :to="btn.link" variant="dark">{{ btn.label }}</Button>
         </div>
       </div>
-      <div>
-        
+      <div class="grid grid-cols-1 md:grid-cols-2 gap-5 pt-10 lg:pt-8">
+        <div v-for="(service, index) in services" :key="index">
+          <div>
+            <div class="p-5 lg:p-8 rounded-5 border border-[#DDD] h-full">
+              <div class="w-8 h-8 rounded-[12px] bg-[#ddd]"></div>
+              <h1 class="text-4 lg:text-6 2xl:text-9 font-primary py-2 md:py-4">
+                {{ service.title }}
+              </h1>
+              <p class="text-par">{{ service.desc }}</p>
+            </div>
+          </div>
+        </div>
       </div>
     </main>
   </section>
